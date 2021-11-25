2b6b44d0b24be25d38ccad3845308df34c0bbb25
idadilham <dadiilhamsetiadii@gmail.com>
2021-11-25 21:29:56 +0700

    initial commit

--- script/index.js
+++ script/index.js
@@ -1,21 +1,15 @@
-var pukul = 20; // jam aktivitas
-var aktivitas;
-
-// tentuin aktiivtas
-if (pukul == 5) {
-  aktivitas = "Bangun Tidur";
-} else if (pukul > 5 && pukul < 9) {
-  aktivitas = "Persiapan Pagi (Subuh, Mandi, Sarapan dll)";
-} else if (pukul > 8 && pukul < 12) {
-  aktivitas = "Ngerjain Skripsi, Ngerjain taks magang";
-} else if (pukul > 12 && pukul < 14) {
-  aktivitas = "Istirahat Siang (Zuhur, Makan, dll)";
-} else if (pukul > 14 && pukul < 17) {
-  aktivitas = "Lanjut produktif lagi";
-} else if (pukul > 17 && pukul < 21) {
-  aktivitas = "Belajar, Kadang Istirahat, dll";
-} else {
-  aktivitas = "Tidur";
+function bintangSegitiga(n) {
+  let bintang = ""; // var bintang
+  // loop
+  for (let i = 0; i < n; i++) {
+    // nested loop
+    for (let j = 0; j <= i; j++) {
+      bintang += "* ";
+    }
+    bintang += "\n";
+  }
+  return bintang;
 }
 
-console.log(aktivitas);
+const segitiga = bintangSegitiga(5);
+console.log(segitiga);
